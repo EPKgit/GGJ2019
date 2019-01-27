52296054ff512334e8d446ecca33b1713c07fbcf
Sam <shiehsamantha@gmail.com>
2019-01-27 14:21:34 -0800

    menu and tutorial screen

---
+++ Assets/Scripts/tutorialButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialButton : MonoBehaviour
+{
+    public GameObject canvas; 
+    public void openTutorial()
+    {
+        canvas.SetActive(true);
+    }
+}
