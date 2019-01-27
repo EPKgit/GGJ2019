e4e6cded6b8b235f6d9fd05d3e746a1eb631d3cf
Ben Nguyen <btatnguyen@gmail.com>
2019-01-26 22:55:29 -0800

    New TradeCard metadata?

--- Assets/Scripts/SettleButtonControl.cs
+++ Assets/Scripts/SettleButtonControl.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SettleButtonControl : MonoBehaviour
 {
+    public GameObject confirmationWindow;
+
     void Start()
     {
-        
+        if (confirmationWindow.activeSelf)
+            confirmationWindow.SetActive(false);
+    }
+
+    public void SettlePressed()
+    {
+        confirmationWindow.SetActive(true);
+    }
+
+    public void ConfirmationPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void DeconfirmationPressed()
+    {
+        confirmationWindow.SetActive(false);
     }
 }
