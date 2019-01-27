45c8d8af53cf4299073d300d4892815175b09dbd
Ben Nguyen <btatnguyen@gmail.com>
2019-01-26 22:45:44 -0800

    Making a Settle button.

---
+++ Assets/Scripts/ButtonCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCard : MonoBehaviour
+{
+    public Card card;
+    public Image cardName;
+    public Text cardSuit;
+    public Text abilityText;
+    public Text flavorText;
+    public bool buttonEnabled;
+    //public string cardOwner;
+
+
+}
