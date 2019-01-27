ef3342849c66dec0169216de8a4ccb1d36f68eec
Eric Kim <ericpaulkim@gmail.com>
2019-01-27 15:42:56 -0800

    hop + tp

--- Assets/Scripts/BigCard.cs
+++ Assets/Scripts/BigCard.cs
@@ -10,6 +10,7 @@ public class BigCard : MonoBehaviour
     public Text name;
     public Text effect;
     public Image art;
+    public Image frame;
 
     void Start()
     {
@@ -18,6 +19,10 @@ public class BigCard : MonoBehaviour
             Destroy(gameObject);
         }
         instance = this;
+        frame = transform.GetChild(1).GetComponent<Image>();
+        art = transform.GetChild(2).GetComponent<Image>();
+        name = transform.GetChild(4).GetComponent<Text>();
+        effect = transform.GetChild(3).GetComponent<Text>();
     }
 
     public void setNewBigCard(Card c)
@@ -29,5 +34,24 @@ public class BigCard : MonoBehaviour
         name.text = c.cardName;
         effect.text = c.cardAbilityText;
         art.sprite = c.cardImage;
+        Debug.Log(c.cardSuit);
+        switch(c.cardSuit)
+        {
+            case Card.Suit.DIP: 
+                frame.color = Color.blue;
+                break;
+            case Card.Suit.FOOD: 
+                frame.color = Color.green;
+                break;
+            case Card.Suit.MACHINE: 
+                frame.color = Color.red;
+                break;
+            case Card.Suit.TOY: 
+                frame.color = Color.magenta;//new Color(164f, 122f, 211f);
+                break;
+            case Card.Suit.VALUABLE: 
+                frame.color = Color.yellow;
+                break;
+        }
     }
 }
--- Assets/Scripts/HalfCard.cs
+++ Assets/Scripts/HalfCard.cs
@@ -15,6 +15,24 @@ public class HalfCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         //card = prefabCard.GetComponent<Card>();
         transform.GetChild(1).GetComponent<Image>().sprite = prefabCard.GetComponent<Card>().cardImage;
+        switch(prefabCard.GetComponent<Card>().cardSuit)
+        {
+            case Card.Suit.DIP: 
+                transform.GetChild(2).GetComponent<Image>().color = Color.blue;
+                break;
+            case Card.Suit.FOOD: 
+                transform.GetChild(2).GetComponent<Image>().color = Color.green;
+                break;
+            case Card.Suit.MACHINE: 
+                transform.GetChild(2).GetComponent<Image>().color = Color.red;
+                break;
+            case Card.Suit.TOY: 
+                transform.GetChild(2).GetComponent<Image>().color = Color.magenta;//new Color(164f, 122f, 211f);
+                break;
+            case Card.Suit.VALUABLE: 
+                transform.GetChild(2).GetComponent<Image>().color = Color.yellow;
+                break;
+        }
     }
 
     public void OnPointerEnter(PointerEventData pointerEventData)
